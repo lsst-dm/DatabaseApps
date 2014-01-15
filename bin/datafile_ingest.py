3082fd079bcf416cc3e181a350e4cb3020c08ced
tomashek <tomashek>
2014-01-15 22:54:20 +0000

    fixed notification bug

--- bin/datafile_ingest.py
+++ bin/datafile_ingest.py
@@ -91,8 +91,8 @@ if __name__ == '__main__':
     if(len(sys.argv) > 1):
         filename = sys.argv[1]
         filetype = sys.argv[2]
-        sys.stderr.write("Missing required parameters. Must include filename, filetype")
     else:
+        sys.stderr.write("Missing required parameters. Must include filename, filetype\n")
         exit(1)
 
     try:
