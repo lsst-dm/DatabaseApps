26a8a395864e0f6dcd75d9c3d9bb15097e8b0de1
tomashek <tomashek>
2014-01-17 22:12:48 +0000

    fixed documentation

--- bin/merge_objects.py
+++ bin/merge_objects.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# $Id: catalog_ingest.py 11430 2013-04-12 21:41:16Z tomashek $
+# $Id: merge_objects.py 11430 2014-01-17 21:41:16Z tomashek $
 # $Rev::                                  $:  # Revision of last commit.
 # $LastChangedBy::                        $:  # Author of last commit.
 # $LastChangedDate::                      $:  # Date of last commit.
