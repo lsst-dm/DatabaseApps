ff6e2d93c50ccb79a04d0b106a1e9c79c881539e
tomashek <tomashek>
2014-01-28 17:37:17 +0000

    changed tablespace to request to make name deterministic

--- bin/create_ingest_table.py
+++ bin/create_ingest_table.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create ingest temp table')
     parser.add_argument('--temptable',action='store')
     parser.add_argument('--targettable',action='store')
-    parser.add_argument('--tablespace',action='store')
+    parser.add_argument('--request',action='store')
 
     args, unknown_args = parser.parse_known_args()
     args = vars(args)
@@ -28,19 +28,21 @@ if __name__ == '__main__':
         errors.append("targettable is required")
     if args['temptable'] == None:
         errors.append("temptable is required")
-    if args['tablespace'] == None:
-        errors.append("tablespace is required")
+    if args['request'] == None:
+        errors.append("request is required")
     if len(errors) > 0:
         sys.stderr.write("ERROR: " + "; ".join(errors) + "\n")
         exit(1)
 
+    tablespace = "DESSE_REQNUM%07d_T" % int(args['request'])
+
     dbh = desdbi.DesDbi()
     cursor = dbh.cursor()
-    print "Creating tablespace %s ..." % args['tablespace']
-    cursor.callproc("des_admin.createObjectsTablespace",[args['tablespace']])
+    print "Creating tablespace %s ..." % tablespace
+    cursor.callproc("des_admin.createObjectsTablespace",[tablespace])
 
     print "Creating temp table %s using %s as a template..." % (args['temptable'], args['targettable'])
-    cursor.execute("create table %s tablespace %s as select * from %s where 1=0" % (args['temptable'], args['tablespace'],args['targettable']))
+    cursor.execute("create table %s tablespace %s as select * from %s where 1=0" % (args['temptable'],tablespace,args['targettable']))
     cursor.close()
     print "Temp table %s created successfully" % args['temptable']
 
