7aa72d4befddfc3175e073c0b08e9a85ce0ea0f9
Felipe Menanteau <felipe@illinois.edu>
2016-08-03 21:02:38 +0000

    Getting ready for tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='DatabaseApss',
-      version ='2.0.2',
+      version ='2.0.3',
       license = "GPL",
       description = "Provide DES database access methods",
       author = "Michelle Gower",
