5c1290718d94474a203320b9ea068c9f1e465673
Gregory Daues <daues@illinois.edu>
2016-08-23 03:03:32 +0000

    ready trunk for tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*")
 
 # The main call
 setup(name='DatabaseApss',
-      version ='2.0.3',
+      version ='2.0.4',
       license = "GPL",
       description = "Provide DES database access methods",
       author = "Michelle Gower",
